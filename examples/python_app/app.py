83c66370e4f11119a76a26d813b1f38e2043218a
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-02-22 10:51:20 +0200

    feat: Update and add to Python app

--- examples/python_app/app.py
+++ examples/python_app/app.py
@@ -1,1 +1,15 @@
+import requests
+
+url = "http://httpbin.org/get?message=Hello,%20World!"
+
+print("Standard greeting:")
 print("Hello, World!")
+print()
+
+print(f"Requesting: {url}")
+
+resp = requests.get(url)
+
+print(f"Status: {resp.status_code}")
+print("JSON response:")
+print(resp.json())

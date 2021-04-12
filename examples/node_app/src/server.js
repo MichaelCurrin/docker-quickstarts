2b333e30b61dca3571f0a36ac71d3e2fea2f9346
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-04-12 13:07:45 +0200

    Update server.js

--- examples/node_app/src/server.js
+++ examples/node_app/src/server.js
@@ -6,8 +6,7 @@ const PORT = 80;
 const app = express();
 
 app.get('/', (_req, res) => {
-  res.send('Hello World');
+  res.send('Hello, World!');
 });
 
-console.log(`Starting dev server on http://${HOST}:${PORT}`);
-app.listen(PORT, HOST);
+app.listen(PORT, () => console.log(`Listening at http://localhost:${PORT}`));

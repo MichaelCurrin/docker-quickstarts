e31195c002e6c0e1b60bddfc399d1d013b16c368
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-04-12 13:22:51 +0200

    style: Format

--- examples/node_app/src/server.js
+++ examples/node_app/src/server.js
@@ -1,11 +1,11 @@
-import express from 'express';
+import express from "express";
 
 const PORT = 8080;
 
 const app = express();
 
-app.get('/', (_req, res) => {
-  res.send('Hello, World!');
+app.get("/", (_req, res) => {
+  res.send("Hello, World!");
 });
 
 app.listen(PORT, () => console.log(`Listening at http://localhost:${PORT}`));
